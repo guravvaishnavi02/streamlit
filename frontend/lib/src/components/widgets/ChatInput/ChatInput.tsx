cdaf3cae397e5f4ec9d6739a9799f44525607d8e
Eric Chiu <e.chiu@snowflake.com>
2025-01-15 01:50:36 +0800

    remove redundant borders

--- frontend/lib/src/components/widgets/ChatInput/ChatInput.tsx
+++ frontend/lib/src/components/widgets/ChatInput/ChatInput.tsx
@@ -410,17 +410,6 @@ function ChatInput({
     return scrollHeight
   }
 
-  const getTextAreaBorderStyle = (): React.CSSProperties =>
-    acceptFile !== AcceptFileValue.None
-      ? { border: "none" }
-      : {
-          // Baseweb requires long-hand props, short-hand leads to weird bugs & warnings.
-          borderLeftWidth: theme.sizes.borderWidth,
-          borderRightWidth: theme.sizes.borderWidth,
-          borderTopWidth: theme.sizes.borderWidth,
-          borderBottomWidth: theme.sizes.borderWidth,
-        }
-
   const handleSubmit = (): void => {
     // We want the chat input to always be in focus
     // even if the user clicks the submit button
@@ -556,7 +545,7 @@ function ChatInput({
                 style: {
                   minHeight: theme.sizes.minElementHeight,
                   outline: "none",
-                  ...getTextAreaBorderStyle(),
+                  border: "none",
                 },
               },
               Input: {
